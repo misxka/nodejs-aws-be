78a6ce73ab757886c51c41fb7b1408e431048ac3
Mikhail Viaryha <mishutkavlesu@gmail.com>
2022-06-11 15:05:12 +0300

    feat: wrap utils function in async/await

--- product-service/src/functions/products/getProductById.ts
+++ product-service/src/functions/products/getProductById.ts
@@ -3,10 +3,10 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import { formatJSONResponse } from '../../libs/api-gateway';
 import { middyfy } from '../../libs/lambda';
 import { Product } from '../../models/product.model';
-import products from '../../mocks/products.json';
+import { findProductById } from '../../utils/product';
 
 const getProductById: APIGatewayProxyHandler = async (event) => {
-  const product: Product = products.find(product => product.id === event.pathParameters.productId);
+  const product: Product = await findProductById(event.pathParameters.productId);
   if (!product) {
     return formatJSONResponse(404, {
       message: 'Product not found.'
--- product-service/src/functions/products/getProductsList.ts
+++ product-service/src/functions/products/getProductsList.ts
@@ -2,10 +2,12 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 import { formatJSONResponse } from '../../libs/api-gateway';
 import { middyfy } from '../../libs/lambda';
-import products from '../../mocks/products.json';
+import { Product } from '../../models/product.model';
+import { findAllProducts } from '../../utils/product';
 
 const getProductsList: APIGatewayProxyHandler = async () => {
-  return formatJSONResponse({
+  const products: Product[] = await findAllProducts();
+  return formatJSONResponse(200, {
     products,
   });
 };
