319ac30e3b7367783700ece5670775324c024313
Mikhail Viaryha <mishutkavlesu@gmail.com>
2022-07-03 20:05:44 +0300

    fix: fix bug in authorizer

--- authorization-service/src/functions/basicAuthorizer.ts
+++ authorization-service/src/functions/basicAuthorizer.ts
@@ -5,29 +5,29 @@ enum Actions {
   Deny = 'Deny',
 };
 
-export const basicAuthorizer = async (event: APIGatewayTokenAuthorizerEvent, context, cb: Callback<APIGatewayAuthorizerResult>) => {
+export const basicAuthorizer = (event: APIGatewayTokenAuthorizerEvent, context, cb: Callback<APIGatewayAuthorizerResult>) => {
   if (event.type !== 'TOKEN') {
     cb('Unauthorized');
   }
-
+  
   try {
     const { authorizationToken, methodArn } = event;
-
+    
     const payload = authorizationToken.split(' ')[1];
     const buffer = Buffer.from(payload, 'base64');
     
     const credentials = buffer.toString('utf-8').split(':');
     const username = credentials[0];
     const password = credentials[1];
-
+    
     const secret = process.env[username];
     const effect = (!secret || secret != password) ? Actions.Deny : Actions.Allow;
 
-    const policy = generatePolicy(credentials, effect, methodArn);
+    const policy = generatePolicy(username, effect, '*');
 
     cb(null, policy);
   } catch (err) {
-    cb(`Unauthorized: ${err}`);
+    cb('Unauthorized');
   }
 };
 
